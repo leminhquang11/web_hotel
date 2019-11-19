2860024f2162d70019064e1caf8ddd6b20e5b7e2
leminhquang11 <quangleminh.leminh@gmail.com>
2019-11-19 13:33:46 +0700

    giao dien

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,16 @@ class StaticPagesController < ApplicationController
   def home; end
 
   def room; end
+  def about_us
+  		render "about_us";
+   end
+   def term_condition
+  		render "term_condition";
+   end
+   def privacy_policy
+  		render "privacy_policy";
+   end
+   def room
+  		render "room";
+   end
 end
